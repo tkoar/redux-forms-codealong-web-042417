0fa1dc5e18151e1fd187d2f196870f75eba50ea1
tkoar <terrancekoar@gmail.com>
2017-07-16 15:31:35 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 class CreateTodo extends Component {
+  state = {
+    text: ""
+  }
+
+  handleText = (event) => this.setState({text: event.target.value})
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.store.dispatch({type: 'ADD_TODO',
+    todo: this.state})
+  }
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={this.handleSubmit}>
+          <label>Add Todo</label>
+          <br></br>
+          <input onChange={this.handleText} type="text"></input>
+          <br></br>
+          <br></br>
+          <input type="submit"></input>
+        </form>
+        {this.props.store.getState().todos}
       </div>
     )
   }
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,5 +1,10 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
-  return state;
+export default function manageTodo(state = {todos: []}, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return (Object.assign({}, state, {todos: [...state.todos, action.todo.text]}))
+      break;
+    default:
+      return state;
+  }
+
 }
